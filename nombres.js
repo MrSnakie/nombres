9108221a8ffe8e6c05266bf29e042f9915737d12
Stéphane B <petitloulou96@free.fr>
2017-01-17 16:42:43 +0100

    Upload final files
    It's work ! :smile:

--- nombres.js
+++ nombres.js
@@ -1,3 +1,20 @@
 /* ----------------------------- */
 /*           JAVASCRIPT          */
 /* ----------------------------- */
+
+function GenererNombre() {
+
+	var numCase;
+	var seuil = parseInt(prompt("Combien voulez-vous afficher de nombres ?"));
+
+	if (seuil<=300) {
+		for (numCase=1; numCase<=seuil; numCase++) {
+			document.getElementById('liste').innerHTML += '<li>'+numCase+'</li>';
+		}
+	}
+
+	else {
+		alert("Erreur !");
+		location.reload();
+	}
+}
